434d6fd2a8145b5f9f43434813f02f68687e617c
wiliandechristo <107126741+wiliandechristo@users.noreply.github.com>
2023-11-11 22:44:41 -0300

    Add files via upload

---
+++ Concatenação/concatenação.js
@@ -0,0 +1,9 @@
+// Usando o operador de concatenação '+'
+let string1 = "Olá";
+let string2 = "mundo";
+let resultado1 = string1 + ", " + string2;
+console.log(resultado1); // Saída: Olá, mundo
+
+// Usando o método concat()
+let resultado2 = string1.concat(", ", string2);
+console.log(resultado2); // Saída: Olá, mundo
